35cb2acde9b04940f2eacf26ff8572ebd72165ba
haroldBauti <haroldbautistaa14@gmail.com>
2025-03-18 16:57:23 -0500

    Version Beta

---
+++ ChatRoom/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatRoom
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage (string room,string user,string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task AddToGroup(string room)
+        {
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            
+            //avisar Quien llego
+            await Clients.Group(room).SendAsync("ShowWho",$"Alguien se conecto: {Context.ConnectionId}");
+        }
+    }
+}
---
+++ ChatRoom/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        
+
+        public static Dictionary<int,string> Rooms=
+            new Dictionary<int, string>()
+            {
+                {1,"Sala 1" },
+                {2,"Sala 2" },
+                {3,"Sala 3" }
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room",room);
+        }
+    }
+}
